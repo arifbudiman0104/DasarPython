2ffbd0e93ffd0867315cd7378b550fc4320c04e1
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-11-08 00:55:19 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+start_time = time.time()
+
+print ('Halo nama saya Arif Budiman Arrosyid')
+# ini adalah comment
+""" ini juga comment, tapi multiline """
+a = 10
+print(a)
+
+#compile dengan bytecode
+#ketik di terminal 'python -m py_compile main.py'
+print(time.time() - start_time, 'detik')
+
+
---
+++ variable.py
@@ -0,0 +1,8 @@
+# variable adalah tempat menyimpan data
+
+a = 2020
+b = 1996
+c = a - b
+print('Usia ', c)
+print('Usia ', a)
+print('Usia ', b)
